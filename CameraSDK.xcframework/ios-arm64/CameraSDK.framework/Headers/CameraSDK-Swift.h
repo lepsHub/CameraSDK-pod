5d26e15335ba7bfcc6edf143c562d3a375d6fb71
lepsHub <backup.user.leps@outlook.com>
2025-12-30 14:26:09 +0000

    Release 78.2.17-BETA.1

--- CameraSDK.xcframework/ios-arm64/CameraSDK.framework/Headers/CameraSDK-Swift.h
+++ CameraSDK.xcframework/ios-arm64/CameraSDK.framework/Headers/CameraSDK-Swift.h
@@ -281,6 +281,7 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 #if __has_warning("-Watimport-in-framework-header")
 #pragma clang diagnostic ignored "-Watimport-in-framework-header"
 #endif
+@import ObjectiveC;
 #endif
 
 #endif
@@ -302,6 +303,11 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 #endif
 
 #if defined(__OBJC__)
+
+SWIFT_CLASS("_TtC9CameraSDK30TruvideoSdkCameraConfiguration")
+@interface TruvideoSdkCameraConfiguration : NSObject
+- (nonnull instancetype)init OBJC_DESIGNATED_INITIALIZER;
+@end
 
 #endif
 #if __has_attribute(external_source_symbol)
